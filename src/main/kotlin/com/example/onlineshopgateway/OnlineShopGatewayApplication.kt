d46fa52621562a49e2b9dcbf0476d714e413985f
kelvin_lai <kelvin_lai@fujitsu.com>
2022-11-23 17:04:52 +0800

    Initial commit

---
+++ src/main/kotlin/com/example/onlineshopgateway/OnlineShopGatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.example.onlineshopgateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnlineShopGatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<OnlineShopGatewayApplication>(*args)
+}
